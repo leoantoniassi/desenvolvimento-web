415f04e21fad07343ab35935731951c3703b72e9
leoantoniassi <leoantoniassi9@gmail.com>
2025-05-01 11:33:17 -0300

    ex 9 em JS

--- aula4/js/ex09.js
+++ aula4/js/ex09.js
@@ -1,4 +1,6 @@
 function inicio() {
+  let nome = document.getElementById("nome").value;
+  
   let precoParafuso = 1.5;
   let precoArruela = 2.0;
   let precoPorca = 2.5;
@@ -12,13 +14,14 @@ function inicio() {
   let totalPorca = precoPorca * qntdPorca;
 
   if (qntdArruela == "" && qntdParafuso == "" && qntdPorca == "") {
-    window.alert("Preencha pelo menos 1 item");
+    window.alert("Preencha pelo menos 1 item à venda!");
   } else {
     let total = totalArruela + totalParafuso + totalPorca;
-    document.getElementById("total").innerHTML = `Valor total: R$${total} <br>
-      valores:<br>
-      Valor Total de parafusos R$${totalParafuso}<br>
-      Valor Total de Arruelas: R$${totalArruela}<br>
-      Valor Total de Porcas: R$${totalPorca}`;
+    document.getElementById("total").innerHTML = `Cliente: ${nome} <br>
+    Valor total: R$${total} <br>`;
+
+    let resultP = document.getElementById("totalParafuso").innerHTML = `Valor total em Parafusos: ${totalParafuso}`;
+    let resultA = document.getElementById("totalArruela").innerHTML = `Valor total em Parafusos: ${totalArruela}`;
+    let resultPo = document.getElementById("totalPorca").innerHTML = `Valor total em Parafusos: ${totalPorca}`;
   }
 }
